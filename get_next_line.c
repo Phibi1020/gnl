b561ec2897dd02b4a223bff87aa6a7365d59e960
Phibi1020 <foongjing5@gmail.com>
2022-05-25 11:04:17 +0800

    secind commit

--- get_next_line.c
+++ get_next_line.c
@@ -1,60 +1,74 @@
-#include<stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-char    *ft_read_content(int fd, char *buf, char *s)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ftan <ftan@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/22 21:08:55 by ftan              #+#    #+#             */
+/*   Updated: 2022/05/23 12:10:02 by ftan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_read_content (int fd, char *buf, char *stash)
 {
-    int lines;
-    char    *temp;
-    
-    lines = read(fd, buf, BUFFER_SIZE);
-    while (lines > 0)
-    {
-        buf[lines] = '\0';
-        if (!s)
-            s = ft_strdup("");
-        //assign current address to temp the free it
-        temp = s;
-        s = ft_strjoin(temp, buf);
-        free(temp);
-        if(ft_strchr(buf, '\n'))
-            break;
-        lines = read(fd, buf, BUFFER_SIZE);
-    }
-    return (s);
+	int	lines;
+	char	*temp;
+
+	lines = read(fd, buf, BUFFER_SIZE);
+	while (lines > 0)
+	{
+		buf[lines] = '\0';
+		if (!stash)
+			stash = ft_strdup("");
+		temp = stash;
+		stash = ft_strjoin(temp, buf);
+		free (temp);
+		if (ft_strchr (buf, '\n'))
+			break;
+		lines = read (fd, buf, BUFFER_SIZE);
+	}
+	return (stash);
 }
-char    *ft_sep(char *line)
+
+char	*ft_sep(char *line)
 {
-    
+	char	*return_line;
+	size_t	count;
+
+	count = 0;
+	//count the position of eol
+	while (line[count] != '\n' && line[count] != '\0')
+		count++;
+	if (line[count] == '\0' || line[1] == '\0')
+		return(0);
+	//get the substring of the line bfore the newline
+	return_line = ft_substr(line, count+1, ft_strlen(line) -1);
+	if(*return_line == '\0')
+	{
+		free(return_line);
+		return_line = NULL;
+	}
+	//append eof to line to only return the desired substring
+	line[count + 1] = '\0';
+	//return the position of next string to static
+	return(return_line);
 }
 
-#ifndef O_RDONLY
-# define O_RDONLY 0
-#endif
-char    *ft_get_next_line(int fd){
-    char *t;
-    char *buf;
-    static char *s;
-    // char    *temp;
+char    *get_next_line(int fd){
+	char *t;
+	char *buf;
+	static char *stash;
 
-    buf = (char *) malloc (sizeof(char) * (BUFFER_SIZE + 1));
-    t = ft_read_content(fd, buf, s);
-    //free the temp string used
-    free(buf);
-    buf = NULL;
-    if (!t)
-        return (NULL);
-    s = ft_sep(t);
-    return t;
-    
-}
-
-int main(int arc, char **argv){
-
-    int f_d;
-    (void)arc;
-    f_d = open(argv[1], O_RDONLY);
-    if (f_d > 0){
-        ft_get_next_line(f_d);
-    }
+	buf = (char *) malloc (sizeof(char) * (BUFFER_SIZE + 1));
+	t = ft_read_content(fd, buf, stash);
+	//free the temp string used
+	free(buf);
+	buf = NULL;
+	if (!t)
+		return NULL;
+	stash = ft_sep(t);
+	return (t);    
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: ftan <ftan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 21:14:00 by ftan              #+#    #+#             */
-/*   Updated: 2022/05/22 21:14:40 by ftan             ###   ########.fr       */
+/*   Updated: 2022/05/23 11:48:46 by ftan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#include<stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_strlen(const char *str);
