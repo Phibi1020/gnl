d61d3100b28d8cab99695d39f262980810c3b8ee
Phibi1020 <foongjing5@gmail.com>
2022-05-28 14:19:31 +0800

    Commented ver

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,28 @@
 /*   By: ftan <ftan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 21:08:55 by ftan              #+#    #+#             */
-/*   Updated: 2022/05/25 14:18:55 by ftan             ###   ########.fr       */
+/*   Updated: 2022/05/27 20:37:28 by ftan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_read_content (int fd, char *buf, char *stash)
+static char	*ft_read_content (int fd, char *buf, char *stash)
 {
 	int		lines;
 	char	*temp;
 
+	//get the first string of buffer size
 	lines = read(fd, buf, BUFFER_SIZE);
+	//if lines is not 0 means there is a string lines are the number of bytes read
 	while (lines > 0)
 	{
+		//put eol in buffer
 		buf[lines] = '\0';
+		//strdup is just to malloc
 		if (!stash)
 			stash = ft_strdup("");
+		//to join sting together 
 		temp = stash;
 		stash = ft_strjoin(temp, buf);
 		free (temp);
@@ -39,18 +44,24 @@ char	*ft_sep(char *line)
 	size_t	count;
 
 	count = 0;
+	//count how many characters are there in first line; check for new line separator to stop
 	while (line[count] != '\n' && line[count] != '\0')
 		count++;
+	//in the case where passed line is a blank
 	if (line[count] == '\0' || line[1] == '\0')
 		return (0);
+	//return line is actually the next line
 	return_line = ft_substr(line, count + 1, ft_strlen(line) - 1);
+	//if the returned line is empty then free the memory
 	if (*return_line == '\0')
 	{
 		free (return_line);
 		return_line = NULL;
 	}
-	line[count + 1] = '\0';
-	return (return_line);
+	//add a EOL symbol to cut desired string
+	line[count + 1] = '\0'; 
+	//return the next line to the static variable
+	return (return_line); 
 }
 
 char	*get_next_line(int fd)
@@ -65,6 +76,13 @@ char	*get_next_line(int fd)
 	buf = NULL;
 	if (!t)
 		return (NULL);
-	stash = ft_sep(t);
+	stash = ft_sep(t); 
 	return (t);
 }
+
+// int main()
+// {
+// 	int fd = open("text.txt", O_RDONLY);
+//  	printf("%s\n",get_next_line(fd));
+// 	printf("Hello World");
+// }
